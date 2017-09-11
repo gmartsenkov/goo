cf2d1adc4b599ba9254fb62f544593e7170890d1
Martsenkov, Georgi, Vodafone Group <georgi.martsenkov@vodafone.com>
2017-09-11 10:17:52 +0100

    remove close from window

--- windows/window.go
+++ windows/window.go
@@ -2,15 +2,9 @@ package windows
 
 import (
 	"goo/common"
-
-	termbox "github.com/nsf/termbox-go"
 )
 
 type Window struct {
 	content    []byte
 	dimensions common.Dimensions
-}
-
-func (window *Window) Close() {
-	termbox.Close()
 }
